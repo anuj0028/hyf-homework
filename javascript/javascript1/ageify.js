f2eb720891e9380e3afbb8d780bd7e7cda932067
anuj0028 <anujkumal75@gmail.com>
2019-03-10 00:35:08 +0100

    added ageify.js

---
+++ javascript/javascript1/ageify.js
@@ -0,0 +1,9 @@
+// Age-ify (A future age calculator)
+
+var yearOfBirth = 1993;
+
+var yearFuture = 2027;
+
+var age = yearFuture - yearOfBirth
+
+console.log("You will be" +age+ "years old in 2027")    
