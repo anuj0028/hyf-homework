26ef961ae1b1a7ff1ad660bfc21a9cba262d2c57
anuj0028 <anujkumal75@gmail.com>
2019-03-24 02:07:56 +0100

    updated studentManager.js

--- javascript/javascript1/week2/studentManager.js
+++ javascript/javascript1/week2/studentManager.js
@@ -1,33 +1,39 @@
 const class07Students = [];
+function getNumberOfStudents() {
+    return class07Students.length;// You write code here
+}
 function addStudentToClass(studentName) {
     if (studentName=== "" ){
         console.log("You cannot add an empty string to a class");
     }
-    else if (studentName!=studentName || class07Students.length<6) {
-            class07Students.push(studentName);
+    else {
+        if(!class07Students.includes(studentName)){
+            if(getNumberOfStudents() <6 || studentName.toLowerCase() ==='queen'){
+                class07Students.push(studentName);
+                console.log(class07Students);
+            }else{
+                console.log("You cannot add an more than 6 students");
+            }
+        }else{
+            console.log("Student "+ studentName +" is already in the class'");
         }
-    else if (studentName==='Queen') {
-            class07Students.push('Queen');
-        }
-    else if (studentName===studentName){
-        console.log(studentName+" is already in class or sit limit is only six.");
+       
+       
     }
-    
     // You write code here
 }
-addStudentToClass('Ashish');
-addStudentToClass("John");
-addStudentToClass("Mary");
-addStudentToClass("Eli");
-addStudentToClass("Illu");
-addStudentToClass("Ashish");
+ addStudentToClass('Ashish');
+ addStudentToClass('Ashish');
+ addStudentToClass("John");
+ addStudentToClass("Mary");
+ addStudentToClass("Eli");
+addStudentToClass("Illu"); 
 addStudentToClass("Ram");
-addStudentToClass("Extra");
+addStudentToClass("Ram");
 addStudentToClass("Anuj");
 addStudentToClass("");
-addStudentToClass('Queen');
-console.log(class07Students);
-function getNumberOfStudents() {
-    return class07Students.length;// You write code here
-}
+addStudentToClass("Extra");
+addStudentToClass("Ram");
+addStudentToClass("queen");
+
 console.log(getNumberOfStudents());
