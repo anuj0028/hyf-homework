1acccf95791dad1c92f5d91d4e715b5942a8db0a
anuj0028 <anujkumal75@gmail.com>
2019-03-24 02:04:02 +0100

    updated eventApplication.js

--- javascript/javascript1/week2/eventApplication.js
+++ javascript/javascript1/week2/eventApplication.js
@@ -1,7 +1,5 @@
 function getEventWeekday(eventday) {
     var weekdays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-    todaysDate = new Date();
-    console.log(todaysDate);
     var today = new Date().getDay();
     var mainDate = (today+eventday)%7;
     return weekdays[mainDate];
